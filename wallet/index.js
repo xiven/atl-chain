763ce375146d4526431a3b25d2195708416636d9
JONATHAN <josterman@jonathans-imac.attlocal.net>
2018-06-27 19:44:05 -0400

    Added Mine Transactions, Reward Miners, and Calculate Wallet Balance

--- wallet/index.js
+++ wallet/index.js
@@ -19,7 +19,9 @@ class Wallet {
     return this.keyPair.sign(dataHash);
   }
 
-  createTransaction(recipient, amount, transactionPool) {
+  createTransaction(recipient, amount, blockchain, transactionPool) {
+    this.balance = this.calculateBalance(blockchain);
+
     if (amount > this.balance) {
       console.log(`Amount: ${amount} exceeds current balance: ${this.balance}`);
       return;
@@ -35,6 +37,52 @@ class Wallet {
     }
 
     return transaction;
+  }
+
+  static blockchainWallet() {
+    const blockchainWallet = new this();
+    blockchainWallet.address = 'blockchain-wallet';
+    return blockchainWallet;
+  }
+  
+  calculateBalance(blockchain) {
+    let balance = this.balance;
+    let transactions = [];
+    // run a loop on each block in blockchain
+    // run a loop and look at each transaction at a time
+    blockchain.chain.forEach(block => block.data.forEach(transaction => {
+      transactions.push(transaction);
+    }));
+    // create a smaller set of transactions in this array, 
+    // only return transactions that were inputted by this wallet
+    const walletInputTs = transactions.filter(transaction => transaction.input.address === this.publicKey);
+
+    let startTime = 0;
+
+    if (walletInputTs.length > 0) {
+      // get most recent input transaction
+      const recentInputT = walletInputTs.reduce(
+        (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current
+      );
+      // change balance to the one dedicated in that specified amount as
+      // the result of that transaction
+      balance = recentInputT.outputs.find(output => output.address === this.publicKey).amount;
+      // modify the start time so that when you add up the outputs
+      // you only add up those that come after the recent transaction
+      startTime = recentInputT.input.timestamp;
+    }
+
+    transactions.forEach(transaction => {
+      if(transaction.input.timestamp > startTime) {
+        transaction.outputs.find(output => {
+          if (output.address === this.publicKey) {
+            balance += output.amount;
+          }
+        });
+      }
+    });
+
+    return balance;
   }
 }
 
